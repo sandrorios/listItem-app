5ab64b8c2de2c7db1eba5df5a2883e2a3d4d0c58
sandrorios <sandroderios@hotmail.com>
2023-10-30 11:25:35 -0400

    added codes to the script file

---
+++ listItem-app/script.js
@@ -0,0 +1,7 @@
+let formItem = document.getElementById('list-item');
+let inputField = document.getElementById('input-field');
+let listItem = document.getElementById('list-item');
+let btn = document.querySelector('.btn');
+let filter = document.getElementById('filter');
+let clearBtn = document.getElementById('clear');
+
