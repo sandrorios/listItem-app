a3d02b818bbbfb585df6386626a64ed46de8ac07
sandrorios <sandroderios@hotmail.com>
2023-10-31 11:04:10 -0400

    Added an alert

--- listItem-app/script.js
+++ listItem-app/script.js
@@ -1,7 +1,18 @@
-let formItem = document.getElementById('list-item');
+let formItem = document.getElementById('form-item');
 let inputField = document.getElementById('input-field');
 let listItem = document.getElementById('list-item');
 let btn = document.querySelector('.btn');
 let filter = document.getElementById('filter');
 let clearBtn = document.getElementById('clear');
 
+function addItem(e) {
+    e.preventDefault();
+    let newItem = inputField.value;
+    if(newItem === ''){
+        alert("Please add an item");
+        return;
+    }
+}
+
+
+formItem.addEventListener('click', addItem);
